c0c26cfcb9362f0d4b05b7ca4f33d5bd251120da
taocx <taocx@gviet.vn>
2024-03-31 18:53:31 +0700

    feat: feat extract id

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <NuxtLayout>
+    <NuxtLoadingIndicator />
+    <DefaultLayout>
+      <NuxtPage />
+    </DefaultLayout>
+  </NuxtLayout>
+</template>
+<style>
+body {
+  background-color: rgb(190, 211, 230);
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/pages/feedback/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        Feedback
+    </div>
+</template>
+
+
+
+<style scoped></style>
